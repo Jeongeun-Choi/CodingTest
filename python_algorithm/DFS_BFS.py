9464bf0f63d31f19c2c70b20dd7bdafc24c69bf9
Jeongeun-Choi <cje3080@gmail.com>
2019-11-29 00:47:16 +0900

    백준 DFS_BFS

--- python_algorithm/DFS_BFS.py
+++ python_algorithm/DFS_BFS.py
@@ -1,4 +1,12 @@
 N, M, V = map(int, input().split())
 
+arr = []
+
 for i in range(M):
-    
+    arr.append(list(map(int, input().split()))) #append 좀 써라
+
+# print(arr[0][0])
+# print(arr[0][1])
+
+#처음이 V인거부터 시작 
+
