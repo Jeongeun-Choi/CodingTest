7f5a3f995c9ec7f47b5fde6a542be9fb60485121
Jeongeun-Choi <cje3080@gmail.com>
2021-03-10 16:55:41 +0900

    백준 계단 오르기

---
+++ python_algorithm/Dynamic/BJ2579.py
@@ -0,0 +1,15 @@
+N = int(input())
+stairs = [0] * (301)
+dp = [0] * (301)
+for index in range(N):
+    stairs[index] = int(input())
+
+dp[0] = stairs[0]
+dp[1] = stairs[0] + stairs[1]
+dp[2] = max(stairs[1] + stairs[2], stairs[0] + stairs[2])
+
+for i in range(3, N):
+    dp[i] = max(dp[i-3] + stairs[i-1] + stairs[i], dp[i-2] + stairs[i])
+
+
+print(dp[N-1])
