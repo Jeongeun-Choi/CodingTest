5b61552db7efad8d11ed43d65e84a3b4169470d3
Jeongeun-Choi <cje3080@gmail.com>
2021-03-10 18:55:04 +0900

    백준 가장 긴 증가하는 부분 수열

---
+++ python_algorithm/Dynamic/BJ11053.py
@@ -0,0 +1,12 @@
+N = int(input())
+A = list(map(int, input().split()))
+
+dp = [1]*N
+
+for i in range(N):
+    for j in range(0, i+1):
+        if A[j] < A[i]:
+            dp[i] = max(dp[i], dp[j]+1)
+
+
+print(max(dp))
