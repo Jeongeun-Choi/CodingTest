ba374d88fcfdfac9776da56b0fc41caac28ac913
Jeongeun-Choi <cje3080@gmail.com>
2021-03-10 11:42:46 +0900

    백준 N과 M(2)

---
+++ python_algorithm/백트래킹/BJ15650.py
@@ -0,0 +1,20 @@
+N, M = map(int, input().split())
+
+lists = [0] * (N+1)
+checked = [False] * (N+1)
+
+def dfs(cnt):
+    if cnt == M + 1:
+        for index in range(1, M + 1):
+            print(lists[index], end=' ')
+        print('')
+        return
+    
+    for index in range(1, N + 1):
+        if not checked[index] and lists[cnt - 1] < index:
+            checked[index] = True
+            lists[cnt] = index
+            dfs(cnt + 1)
+            checked[index] = False
+
+dfs(1)
