6c7180584aa24ac40d5499ec684e78f39b988f81
Jeongeun-Choi <cje3080@gmail.com>
2021-03-10 15:30:04 +0900

    백준 쉬운 계단 수

--- python_algorithm/Dynamic/BJ10844.py
+++ python_algorithm/Dynamic/BJ10844.py
@@ -1,12 +1,16 @@
 N = int(input())
+dp = [[0 for i in range(10)] for i in range(101)]
 
-dp = [0] * (N+1)
-dp[1] = 9
+for index in range(1, 10):
+    dp[1][index] = 1
 
-if N == 1:
-    print(dp[1])
-else :
-    for i in range(2, N+1):
-        dp[i] = dp[i-1] * 2 - (i-1)
-
-print(dp[N])
+for i in range(2, N + 1):
+    for j in range(10):
+        if j == 0:
+            dp[i][j] = dp[i-1][1]
+        elif j == 9:
+            dp[i][j] = dp[i-1][8]
+        else:
+            dp[i][j] = dp[i-1][j-1] + dp[i-1][j+1]
+    
+print(sum(dp[N]) % 1000000000)
